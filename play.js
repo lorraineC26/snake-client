dd23787539651d55ab3c9465ff2ed55ddffe72fe
lorraineC26 <lorraine.quinyee@gmail.com>
2023-09-27 20:54:07 -0400

    Input modeule created

--- play.js
+++ play.js
@@ -1,25 +1,9 @@
 // establishes a connection with the game server
 const connect = require('./client');
-
-// check for the ctrl + c input and terminate the game
-const handleUserInput = function(key) {
-  if (key === '\u0003') { // \u0003 maps to ctrl+c input
-    process.exit();
-  }
-};
+// setup interface to handle user input from stdin
+const setupInput = require('./input');
 
 console.log("Connecting ...");
 connect();
-
-// setup interface to handle user input from stdin
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
 
 setupInput();
